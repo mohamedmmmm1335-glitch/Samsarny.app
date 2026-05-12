3f36b3b1b499ce7ff7dea01e7dcf9558476ad97f
mohamedmmmm1335-glitch <mohamedmmmm1335@gmail.com>
2026-05-12 14:57:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,10 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
+  apiKey: 'AIzaSyAUt5G3o2CRa_hW3TF6aoRDqlj00h_duiY',
+  authDomain: 'samsarny-5a5c2.firebaseapp.com',
+  databaseURL: 'https://samsarny-5a5c2-default-rtdb.firebaseio.com',
   projectId: 'samsarny-5a5c2',
+  storageBucket: 'samsarny-5a5c2.firebasestorage.app',
   messagingSenderId: '891182543023',
-  apiKey: 'AIzaSyExample', // مش مهم للـ SW
-  appId: '1:891182543023:web:samsarny'
+  appId: '1:891182543023:web:a86920f36595c11a3f645a',
+  measurementId: 'G-TGT9PCJGM9'
 });
 
 const messaging = firebase.messaging();
